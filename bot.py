7ccd9c8c64568745c4a69fa1f439aed29936be6e
Ksarqu <ksarqu@gmail.com>
2021-05-06 15:33:18 +0200

    fix

--- bot.py
+++ bot.py
@@ -82,7 +82,7 @@ async def iqtest(ctx):
     iq = randint(10, 300)
     message = await ctx.send("Analizuje Twoje wiadomości...")    
     await asyncio.sleep(6)
-    await message.delete()
+    message.delete()
     await ctx.send(f"Twoje IQ wynosi {iq}pkt. :brain:")
 
 
